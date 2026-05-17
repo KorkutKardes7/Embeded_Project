6bde2abd200a033b463443000d55c8ff7cf43376
Atakan Akşit <atakanaksit13211@gmail.com>
2026-05-17 15:19:37 +0300

    testing message passing

--- NodeMCU_LoLin_Esp-12E/src/main.cpp
+++ NodeMCU_LoLin_Esp-12E/src/main.cpp
@@ -3,58 +3,101 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-int counter = 0;
 
-#define NSS  16
-#define RST  18
-#define DIO0 19
+#define NSS  D8
+#define RST  D3
+#define DIO0 D2
 
-boolean runEvery(unsigned long interval)
-{
-  static unsigned long previousMillis = 0;
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
-    return true;
+
+String outgoing;              // outgoing message
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xFF;      // destination to send to
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // interval between sends
+
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
+
+
+void onReceive(int packetSize) {
+  Serial.println("?");
+  if (packetSize == 0) return;          // if there's no packet, return
+  Serial.println("!");
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";                 // payload of packet
+
+  while (LoRa.available()) {            // can't use readString() in callback, so
+    incoming += (char)LoRa.read();      // add bytes one by one
   }
-  return false;
+
+  if (incomingLength != incoming.length()) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
 }
 
-void onTxDone() {
-  Serial.println("TxDone");
-}
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);                   // initialize serial
   while (!Serial);
 
-  Serial.println("LoRa Sender non-blocking Callback");
+  Serial.println("LoRa Duplex with callback");
 
-  LoRa.setPins(NSS, RST, DIO0); //set pins
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(NSS, RST, DIO0);// set CS, reset, IRQ pin
 
-  if (!LoRa.begin(915E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
+  LoRa.setSPIFrequency(8E6);
+
+  if (!LoRa.begin(433E6)) {             // initialize ratio at 433 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
   }
 
-  LoRa.onTxDone(onTxDone);
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
+  Serial.println("LoRa init succeeded.");
 }
 
 void loop() {
-  if (runEvery(5000)) { // repeat every 5000 millis
-
-    Serial.print("Sending packet non-blocking: ");
-    Serial.println(counter);
-
-    // send in async / non-blocking mode
-    LoRa.beginPacket();
-    LoRa.print("hello ");
-    LoRa.print(counter);
-    LoRa.endPacket(true); // true = async / non-blocking mode
-
-    counter++;
+  if (millis() - lastSendTime > interval) {
+    String message = "HeLoRa World!";   // send a message
+    sendMessage(message);
+    Serial.println("Sending " + message);
+    lastSendTime = millis();            // timestamp the message
+    interval = random(2000) + 1000;     // 2-3 seconds
+    LoRa.receive();                     // go back into receive mode
   }
 }
-
-
